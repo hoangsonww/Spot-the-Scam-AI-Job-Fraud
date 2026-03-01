4c20d36f11eac55b0c4b24e6476c563cc4712665
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 08:55:07 -0500

    chore: add SOLID refactor note helper

---
+++ refactor-solid-principles-note.js
@@ -0,0 +1,7 @@
+const repoName = 'Spot-the-Scam-AI-Job-Fraud';
+
+function describeSolidRefactorNote() {
+  return `${repoName}: prefer small modules with one clear responsibility.`;
+}
+
+module.exports = { describeSolidRefactorNote };
